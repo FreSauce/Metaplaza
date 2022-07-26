92802e73cb485ec6f66b555aa4e63c55fed35af7
suvanth <saisuvanth@gmail.com>
2022-07-26 23:14:20 +0530

    hehe

--- Assets/CartMenu.cs
+++ Assets/CartMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.UI;
 using TMPro;
 
@@ -7,6 +8,9 @@ public class CartMenu : MonoBehaviour
     public static bool MenuOpen = false;
 
     public GameObject cartMenu;
+    [SerializeField]
+    public GameObject cartItemPrefab;
+    public List<GameObject> cartItems;
 
     private void SetCursorState(bool newState)
     {
@@ -27,4 +31,40 @@ public class CartMenu : MonoBehaviour
         MenuOpen = false;
         SetCursorState(true);
     }
+
+    public void setItems(ICartItem[] items)
+    {
+        foreach(ICartItem item in items)
+        {
+            Debug.Log(item.ToString());
+            GameObject temp = Instantiate(cartItemPrefab);
+            temp.transform.SetParent(cartMenu.transform);
+            temp.transform.localScale = Vector3.one;
+            CartItem ct = temp.GetComponent<CartItem>();
+            ct.name.text = item.name;
+            ct.price.text = item.price;
+            ct.quantity.text = item.quantity.ToString();
+            cartItems.Add(temp);
+        }
+
+    }
+}
+
+[System.Serializable]
+public class CartResponse
+{
+    public string status;
+    public ICartItem[] data;
+}
+
+[System.Serializable]
+public class ICartItem
+{
+    public string _id;
+    public string name;
+    public string link;
+    public string img;
+    public string price;
+    public int quantity;
+    public int __v;
 }
