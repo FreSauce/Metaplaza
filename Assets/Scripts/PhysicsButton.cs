a57f702333bd03ba30767dfd707f9f8ec7fa5820
khanmn <khanmohdfaisal75@gmail.com>
2022-07-28 01:58:32 +0530

    added buttons

--- Assets/Scripts/PhysicsButton.cs
+++ Assets/Scripts/PhysicsButton.cs
@@ -5,92 +5,55 @@ using UnityEngine.Events;
 
 public class PhysicsButton : MonoBehaviour
 {
-    public Rigidbody buttonTopRigid;
-    public Transform buttonTop;
-    public Transform buttonLowerLimit;
-    public Transform buttonUpperLimit;
-    public float threshHold;
-    public float force = 10;
-    private float upperLowerDiff;
-    public bool isPressed;
-    private bool prevPressedState;
-    public AudioSource pressedSound;
-    public AudioSource releasedSound;
-    public Collider[] CollidersToIgnore;
-    public UnityEvent onPressed;
-    public UnityEvent onReleased;
+    [SerializeField] private float threshold = .1f;
+    [SerializeField] private float deadZone = 0.025f;
+    private bool _isPressed;
+    private Vector3 _startPos;
+    private ConfigurableJoint _joint;
 
-    // Start is called before the first frame update
-    void Start()
+    public UnityEvent onPressed, onReleased;
+
+
+
+    private void Start()
     {
-        Collider localCollider = GetComponent<Collider>();
-        if (localCollider != null)
-        {
-            Physics.IgnoreCollision(localCollider, buttonTop.GetComponentInChildren<Collider>());
-
-            foreach (Collider singleCollider in CollidersToIgnore)
-            {
-                Physics.IgnoreCollision(localCollider, singleCollider);
-            }
-        }
-
-        if (transform.eulerAngles != Vector3.zero)
-        {
-            Vector3 savedAngle = transform.eulerAngles;
-            transform.eulerAngles = Vector3.zero;
-            upperLowerDiff = buttonUpperLimit.position.y - buttonLowerLimit.position.y;
-            transform.eulerAngles = savedAngle;
-        }
-        else
-            upperLowerDiff = buttonUpperLimit.position.y - buttonLowerLimit.position.y;
+        _startPos = transform.localPosition;
+        _joint = GetComponent<ConfigurableJoint>();
     }
 
-    // Update is called once per frameW
-    void Update()
+    private void Update()
     {
-        buttonTop.transform.localPosition = new Vector3(0, buttonTop.transform.localPosition.y, 0);
-        buttonTop.transform.localEulerAngles = new Vector3(0, 0, 0);
-        if (buttonTop.localPosition.y >= 0)
-            buttonTop.transform.position = new Vector3(buttonUpperLimit.position.x, buttonUpperLimit.position.y, buttonUpperLimit.position.z);
-        else
-            buttonTopRigid.AddForce(buttonTop.transform.up * force * Time.deltaTime);
-
-        if (buttonTop.localPosition.y <= buttonLowerLimit.localPosition.y)
-            buttonTop.transform.position = new Vector3(buttonLowerLimit.position.x, buttonLowerLimit.position.y, buttonLowerLimit.position.z);
-
-
-        if (Vector3.Distance(buttonTop.position, buttonLowerLimit.position) < upperLowerDiff * threshHold)
-            isPressed = true;
-        else
-            isPressed = false;
-
-        if (isPressed && prevPressedState != isPressed)
+        if (!_isPressed && GetValue()+ threshold >= 1)
+        {
             Pressed();
-        if (!isPressed && prevPressedState != isPressed)
+        }
+        if (!_isPressed && GetValue() - threshold <= 0)
+        {
             Released();
+        }
     }
 
-    // void FixedUpdate(){
-    //     Vector3 localVelocity = transform.InverseTransformDirection(buttonTop.GetComponent<Rigidbody>().velocity);
-    //     Rigidbody rb = buttonTop.GetComponent<Rigidbody>();
-    //     localVelocity.x = 0;
-    //     localVelocity.z = 0;
-    //     rb.velocity = transform.TransformDirection(localVelocity);
-    // }
-
-    void Pressed()
+    public float GetValue()
     {
-        prevPressedState = isPressed;
-        pressedSound.pitch = 1;
-        pressedSound.Play();
+        var value = Vector3.Distance(_startPos, transform.localPosition) / _joint.linearLimit.limit;
+
+        if(Mathf.Abs(value) < deadZone)
+        {
+            value = 0;
+        }
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+
+    private void Pressed()
+    {
+        _isPressed = true;
         onPressed.Invoke();
+        Debug.Log("Pressed");
     }
-
-    void Released()
+    public void Released()
     {
-        prevPressedState = isPressed;
-        releasedSound.pitch = Random.Range(1.1f, 1.2f);
-        releasedSound.Play();
+        _isPressed=false;
         onReleased.Invoke();
+        Debug.Log("Released");
     }
 }
